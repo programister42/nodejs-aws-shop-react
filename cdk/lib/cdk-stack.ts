18e3be9cc04524bdd4ed88681da270bde4336b1e
Taras Tereschenko <tarrrasio@gmail.com>
2024-06-24 21:32:40 +0500

    fix: FIX EVERYTHING

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,14 +1,53 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
+import {aws_cloudfront, aws_iam, aws_s3, aws_s3_deployment, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-
-import {StaticSite} from './static-site';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends Stack {
+export class NodejsAWSShopFrontendStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const staticSite = new StaticSite(this, 'programister42-static-site');
+        const cloudfrontOAI = new aws_cloudfront.OriginAccessIdentity(this, 'NodejsAWSShopCloudfrontOAI');
+
+        const siteBucket = new aws_s3.Bucket(this, 'NodejsAWSShopStaticBucket', {
+            bucketName: 'programister42-nodejs-aws-shop-static-site',
+            websiteIndexDocument: 'index.html',
+            blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL
+        });
+        siteBucket.addToResourcePolicy(new aws_iam.PolicyStatement({
+            actions: ['s3:GetObject'],
+            resources: [siteBucket.arnForObjects('*')],
+            principals: [new aws_iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+        }));
+
+        const distribution = new aws_cloudfront.CloudFrontWebDistribution(this, 'NodejsAWSShopCloudfrontDistribution', {
+            originConfigs: [{
+                s3OriginSource: {
+                    s3BucketSource: siteBucket,
+                    originAccessIdentity: cloudfrontOAI
+                },
+                behaviors: [{isDefaultBehavior: true}]
+            }],
+            errorConfigurations: [
+                {
+                    errorCode: 403,
+                    responseCode: 200,
+                    responsePagePath: '/index.html'
+                },
+                {
+                    errorCode: 404,
+                    responseCode: 200,
+                    responsePagePath: '/index.html'
+                }
+            ]
+        });
+
+        new aws_s3_deployment.BucketDeployment(this, 'NodejsAWSShopBucketDeployment', {
+            sources: [aws_s3_deployment.Source.asset('../dist')],
+            destinationBucket: siteBucket,
+            distribution,
+            distributionPaths: ['/*']
+        });
+
     }
 }
