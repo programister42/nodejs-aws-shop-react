b5595dad28689bb8ed17c67ed6a0d0dc6e5c59dd
Taras Tereschenko <tarrrasio@gmail.com>
2024-08-05 01:30:36 +0500

    feat: add task-9 api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://d93aaj5lj9.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://programister42-cart-api-prod2.us-east-1.elasticbeanstalk.com",
   import: "https://qdde42rit0.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://programister42-cart-api-prod2.us-east-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
