34594acbd6c216712cc754c9904a3907f2eee0f6
Taras Tereschenko <tarrrasio@gmail.com>
2024-08-07 14:24:31 +0500

    refactor: change task-9 api paths to cloudfront

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://d93aaj5lj9.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://programister42-cart-api-prod2.us-east-1.elasticbeanstalk.com",
+  order: "https://dp9vqoib6xd1l.cloudfront.net",
   import: "https://qdde42rit0.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://programister42-cart-api-prod2.us-east-1.elasticbeanstalk.com",
+  cart: "https://dp9vqoib6xd1l.cloudfront.net",
 };
 
 export default API_PATHS;
