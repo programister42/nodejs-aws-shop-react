293685d8b43007a359e15cfe2b5036a25cdb2552
Taras Tereschenko <tarrrasio@gmail.com>
2024-08-11 18:50:11 +0500

    Task 9 (Docker and AWS Elastic Beanstalk) (#6)
    * feat: add task-9 api paths
    
    * refactor: change task-9 api paths to cloudfront
    
    * refactor: change task-9 api paths to cloudfront

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://d93aaj5lj9.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://dp9vqoib6xd1l.cloudfront.net/api",
   import: "https://qdde42rit0.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://dp9vqoib6xd1l.cloudfront.net/api",
 };
 
 export default API_PATHS;
